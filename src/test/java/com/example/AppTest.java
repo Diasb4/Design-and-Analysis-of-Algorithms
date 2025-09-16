da51a0002eccb7dd15aee9769a3d4cce8a09482f
Diasb4 <bbdng6@gmail.com>
2025-09-16 21:47:09 +0500

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void additionShouldWork() {
+        int result = 2 + 3;
+        assertEquals(5, result, "2 + 3 должно быть равно 5");
+    }
+}
